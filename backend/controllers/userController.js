5e9be5bb340b174defdc4d43fcfe838568a27ff4
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-08-03 19:30:55 -0300

    create login in controlerModel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,10 +14,10 @@ const getAllUsers = async (_req, res) => {
   }
 };
 
-//Add check if email already exist 
+//Add check if email already exist
 // Add data format check
 
-const registerUser = async (req, res) => { 
+const registerUser = async (req, res) => {
   try {
     const { user_name, email, user_password } = req.body;
 
@@ -39,7 +39,31 @@ const registerUser = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  try {
+    const { email, user_password } = req.body;
+    const userFromDB = await User.findOne({ where: { email } });
+
+    const isPasswordValid = await bcrypt.compare(
+      user_password,
+      userFromDB.user_password,
+    );
+    if (!userFromDB || !isPasswordValid) {
+      return res.status(400).json({ message: 'Invalid login and/or password' });
+    }
+
+    const { user_password: _, ...userWithoutPassword } = userFromDB;
+    const token = await createToken(userWithoutPassword);
+
+    return res.status(201).json({ token }); //201 ????
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).send({ message: 'Error to login process' });
+  }
+};
+
 module.exports = {
   getAllUsers,
   registerUser,
+  login
 };
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,11 +1,10 @@
 const { Router } = require('express');
 const { userController } = require('../controllers');
 
-
 const userRouter = Router();
 
 userRouter.get('/', userController.getAllUsers);
 userRouter.post('/register', userController.registerUser);
-// userRouter.post('/login', userController.login);
+userRouter.post('/login', userController.login);
 
 module.exports = { userRouter };
