5845e6f4c42eea3aa56d88b6820892edc884942c
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-08-03 07:09:10 -0300

    migration create-users

---
+++ backend/migrations/20210803100720-create-users.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const userTable = queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      user_password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: new Date(),
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: new Date(),
+        type: Sequelize.DATE,
+      },
+    });
+
+    return userTable;
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.dropTable('Users');
+  },
+};
