1e406116189fc9d88524ac626c4d5bd6fe992e8c
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-08-05 19:20:06 -0300

    create edit (put)  post tests

--- backend/tests/post.test.js
+++ backend/tests/post.test.js
@@ -106,7 +106,7 @@ describe('Tests the endpoint `/post`', () => {
       });
   });
 
-  it.skip('It is not possible to post e new Post with a blank content', async () => {
+  it('It is not possible to post e new Post with a blank content', async () => {
     await frisby
       .post(`${url}/user/login`, {
         email: 'johndoe@gmail.com',
@@ -281,7 +281,11 @@ describe('Tests the endpoint `/post`', () => {
         },
       })
       .delete(`${url}/post/1`)
-      .expect('status', 401);
+      .expect('status', 401)
+      .then((response) => {
+        const { json } = response;
+        expect(json.message).toBe('missing auth token');
+      });
   });
 
   it.skip('It is not possible to delete a Post with a wrong token', async () => {
@@ -303,58 +307,201 @@ describe('Tests the endpoint `/post`', () => {
         },
       })
       .delete(`${url}/post/1`)
-      .expect('status', 401);
+      .expect('status', 401)
+      .then((response) => {
+        const { json } = response;
+        expect(json.message).toBe('jwt malformed');
+      });
   });
 
-  // it.skip('It is possible to update a Post', async () => {
-  //   const challengeQuery = readFileSync(
-  //     './tests/sqlQueryTests/getAllPosts.sql',
-  //     'utf8',
-  //   ).trim();
-  //   const expectedResult = require('./resultsQueryTests/getAllPostsReturn');
-  //   await frisby
-  //     .post(`${url}/user/login`, {
-  //       email: 'johndoe@gmail.com',
-  //       user_password: '123456',
-  //     })
-  //     .expect('status', 201)
-  //     .then((response) => {
-  //       const { json } = response;
-  //       expect(json.token).not.toBeNull();
-  //       token = json.token;
-  //     });
+  it.skip('It should be possible to edit a Post ', async () => {
+    let token;
+    await frisby
+      .post(`${url}/user/login`, {
+        email: 'johndoe@gmail.com',
+        user_password: '123456',
+      })
+      .expect('status', 201)
+      .then((response) => {
+        const { body } = response;
+        const result = JSON.parse(body);
+        token = result.token;
+      });
 
-  //   await frisby
-  //     .setup({
-  //       request: {
-  //         headers: {
-  //           Authorization: token,
-  //           'Content-Type': 'application/json',
-  //         },
-  //       },
-  //     })
-  //     .post(`${url}/put`, {
-  //       title: 'The 3thd one',
-  //       content: 'again posting',
-  //     })
-  //     .expect('status', 201)
-  //     .then((response) => {
-  //       const { json } = response;
-  //       expect(json.title).toBe('The 3thd one');
-  //       expect(json.content).toBe('again posting');
-  //       expect(json.userId).toBe(2);
-  //     });
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            Authorization: token,
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .put(`${url}/post/1`, {
+        title: 'post (edited)',
+        content: 'Post edited ....',
+      })
+      .expect('status', 204);
+  });
 
-  //   expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(
-  //     expectedResult,
-  //   );
-  // });
+  it.skip('It should be not possible to edit a Post logged with user different the one who create this post', async () => {
+    let token;
+    await frisby
+      .post(`${url}/user/login`, {
+        email: 'johndoe@gmail.com',
+        user_password: '123456',
+      })
+      .expect('status', 201)
+      .then((response) => {
+        const { body } = response;
+        const result = JSON.parse(body);
+        token = result.token;
+      });
 
-  // it.skip('It is not possible to update a Post with a wrong token', async () => {
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            Authorization: token,
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .put(`${url}/post/2`, {
+        title: 'post (edited)',
+        content: 'Post edited ....',
+      })
+      .expect('status', 401)
+      .then((response) => {
+        const { json } = response;
+        expect(json.message).toBe('user not granted');
+      });
+  });
 
-  //   it.skip('It is not possible to update a Post with a wrong user id', async () => {
+  it.skip('It should be not possible to edit a Post a Post with empty token', async () => {
+    let token;
+    await frisby
+      .post(`${url}/user/login`, {
+        email: 'johndoe@gmail.com',
+        user_password: '123456',
+      })
+      .expect('status', 201);
 
-  //   it.skip('It is not possible to delete a Post with a wrong token', async () => {
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            Authorization: '',
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .put(`${url}/post/1`, {
+        title: 'post (edited)',
+        content: 'Post edited ....',
+      })
+      .expect('status', 401)
+      .then((response) => {
+        const { json } = response;
+        expect(json.message).toBe('missing auth token');
+      });
+  });
 
-  //   it.skip('It is not possible to delete a Post with a wrong user id', async () => {
+
+  it.skip('It should be not possible to edit a Post with a wrong token', async () => {
+    let token;
+    await frisby
+      .post(`${url}/user/login`, {
+        email: 'johndoe@gmail.com',
+        user_password: '123456',
+      })
+      .expect('status', 201);
+
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            Authorization: 'wrongToken',
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .put(`${url}/post/1`, {
+        title: 'post (edited)',
+        content: 'Post edited ....',
+      })
+      .expect('status', 401)
+      .then((response) => {
+        const { json } = response;
+        expect(json.message).toBe('jwt malformed');
+      });
+  });
+
+
+  it('It should not be possible to edit a Post with empty title', async () => {
+    let token;
+    await frisby
+      .post(`${url}/user/login`, {
+        email: 'johndoe@gmail.com',
+        user_password: '123456',
+      })
+      .expect('status', 201)
+      .then((response) => {
+        const { body } = response;
+        const result = JSON.parse(body);
+        token = result.token;
+      });
+
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            Authorization: token,
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .put(`${url}/post/1`, {
+        content: 'Post edited ....',
+      })
+      .expect('status', 400)
+      .then((response) => {
+        const { json } = response;
+        expect(json.message).toBe('"title" is required');
+      });
+  });
+
+  it('It should not be possible to edit a Post with empty content', async () => {
+    let token;
+    await frisby
+      .post(`${url}/user/login`, {
+        email: 'johndoe@gmail.com',
+        user_password: '123456',
+      })
+      .expect('status', 201)
+      .then((response) => {
+        const { body } = response;
+        const result = JSON.parse(body);
+        token = result.token;
+      });
+
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            Authorization: token,
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .put(`${url}/post/1`, {
+        title: 'Post edited',
+      })
+      .expect('status', 400)
+      .then((response) => {
+        const { json } = response;
+        expect(json.message).toBe('"content" is required');
+      });
+  });
+
 });
