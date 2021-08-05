82f60da3a6f6b5ac4da3052b1acaacc550fb95e1
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-08-04 22:04:16 -0300

    add Post tests

--- backend/tests/post.test.js
+++ backend/tests/post.test.js
@@ -3,7 +3,7 @@ const shell = require('shelljs');
 const { Sequelize } = require('sequelize');
 const { readFileSync } = require('fs');
 const dotenv = require('dotenv');
-dotenv.config({ path: './../.env'}) 
+dotenv.config({ path: './../.env' });
 
 const url = `http://${process.env.HOSTNAME}:${process.env.PORT}`;
 
@@ -34,28 +34,27 @@ describe('Tests the endpoint `/post`', () => {
       'utf8',
     ).trim();
     const expectedResult = require('./resultsQueryTests/getAllPostsReturn');
-    console.log("expectedResult",expectedResult);
     await frisby
-    .post(`${url}/user/login`, {
-      email: 'johndoe@gmail.com',
-      user_password: '123456',
-    })
-    .expect('status', 201)
-    .then((response) => {
-      const { json } = response;
-      expect(json.token).not.toBeNull();
-      token = json.token;
-    });
+      .post(`${url}/user/login`, {
+        email: 'johndoe@gmail.com',
+        user_password: '123456',
+      })
+      .expect('status', 201)
+      .then((response) => {
+        const { json } = response;
+        expect(json.token).not.toBeNull();
+        token = json.token;
+      });
 
     await frisby
-    .setup({
-      request: {
-        headers: {
-          Authorization: token,
-          'Content-Type': 'application/json',
+      .setup({
+        request: {
+          headers: {
+            Authorization: token,
+            'Content-Type': 'application/json',
+          },
         },
-      },
-    })
+      })
       .post(`${url}/post`, {
         title: 'The 3thd one',
         content: 'again posting',
@@ -63,7 +62,9 @@ describe('Tests the endpoint `/post`', () => {
       .expect('status', 201)
       .then((response) => {
         const { json } = response;
-        expect(json.token).not.toBeNull();
+        expect(json.title).toBe('The 3thd one');
+        expect(json.content).toBe('again posting');
+        expect(json.userId).toBe(2);
       });
 
     expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(
@@ -71,6 +72,103 @@ describe('Tests the endpoint `/post`', () => {
     );
   });
 
- 
-});
+  it('It is not possible to post e new Post with a blank title', async () => {
+    await frisby
+      .post(`${url}/user/login`, {
+        email: 'johndoe@gmail.com',
+        user_password: '123456',
+      })
+      .expect('status', 201)
+      .then((response) => {
+        const { json } = response;
+        expect(json.token).not.toBeNull();
+        token = json.token;
+      });
 
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            Authorization: token,
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .post(`${url}/post`, {
+        title: '',
+        content: 'again posting',
+      })
+      .expect('status', 400)
+      .then((response) => {
+        const { json } = response;
+        expect(json.message).toBe('"title" is required');
+      });
+  });
+
+  it('It is not possible to post e new Post with a blank content', async () => {
+    await frisby
+      .post(`${url}/user/login`, {
+        email: 'johndoe@gmail.com',
+        user_password: '123456',
+      })
+      .expect('status', 201)
+      .then((response) => {
+        const { json } = response;
+        expect(json.token).not.toBeNull();
+        token = json.token;
+      });
+
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            Authorization: token,
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .post(`${url}/post`, {
+        title: 'The 3thd one',
+        content: '',
+      })
+      .expect('status', 400)
+      .then((response) => {
+        const { json } = response;
+        expect(json.message).toBe('"content" is required');
+      });
+  });
+
+  it('It is not possible to post without token', async () => {
+    await frisby
+      .post(`${url}/post`, {
+        title: 'The 3thd one',
+        content: 'again posting',
+      })
+      .expect('status', 401)
+      .then((response) => {
+        const { json } = response;
+        expect(json.message).toBe('missing auth token');
+      });
+  });
+
+  it('It is not possible to post with a wrong token', async () => {
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            Authorization: 'wrongToken',
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .post(`${url}/post`, {
+        title: 'The 3thd one',
+        content: 'again posting',
+      })
+      .expect('status', 401)
+      .then((response) => {
+        const { json } = response;
+        expect(json.message).toBe('jwt malformed');
+      });
+  });
+});
