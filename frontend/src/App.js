72006b595374832c6e8d39133fb1ebe4a2077da5
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-08-08 17:35:56 -0300

    protect some route with auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import './App.css';
 import Home from './pages/Home/Home';
+import Wall from './pages/Wall/Wall';
+import {checkUserAuthorization} from './services/auth';
 import Register from './pages/Register/Register';
 import PublishPost from './pages/PublishPost/PublishPost';
-import RegisteredSuccessfully from './pages/RegisteredSuccessfully/RegisteredSuccessfully'
-import './App.css';
-import Wall from './pages/Wall/Wall';
+import RegisteredSuccessfully from './pages/RegisteredSuccessfully/RegisteredSuccessfully';
 
 function App() {
   return (
@@ -14,8 +20,18 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/wall" component={Wall} />
-        <Route exact path="/publish" component={PublishPost} />
-        <Route exact path="/registeredsuccessfully" component={RegisteredSuccessfully} />
+        <Route
+          exact
+          path="/publish"
+          render={() =>
+            checkUserAuthorization().authorized ? <PublishPost /> : <Redirect to="/" />
+          }
+        />
+        <Route
+          exact
+          path="/registeredsuccessfully"
+          component={RegisteredSuccessfully}
+        />
         {/* <Route exact path="/login" component={Login} />
         <Route path="/" component={NotFound} /> */}
       </Switch>
