973e1832156d2bc4049eb9937c00064652958693
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-08-08 16:58:32 -0300

    update Wall.jsx - boutons : Logout & home

--- frontend/src/pages/Wall/Wall.jsx
+++ frontend/src/pages/Wall/Wall.jsx
@@ -1,16 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import WallManager from './WallManager';
+import { loadFromLocalStorage, deleteFromLocalStorage } from '../../services/localStorage';
+import jwtDecode from 'jwt-decode';
+
+
 
 
 const Wall = () => {
   const history = useHistory();
   const [isFetching, setIsFetching] = useState(true);
+  const [isAuthorized, setIsAuthorized] = useState(true);
   const [posts, setPosts] = useState([]);
 
+  const checkUserAuthorization = () => {
+    const token = loadFromLocalStorage('WallAppToken')
+
+    if (token != null) {
+      console.log("token :", token);
+      const decoded = jwtDecode(token);
+      const now = Date.now().valueOf() / 1000;
+      if (typeof decoded.exp !== 'undefined' && decoded.exp > now) {
+        const user = {
+          id: "id",
+          name: "userData.name",
+          email: "userData.email",
+        };
+        return { authorized: true, user };
+      }
+    }
+    return { authorized: false, user: "" };
+
+  }
 
   useEffect(() => {
-    console.log("useeffect");
+    const test = checkUserAuthorization()
+    console.log("test", test);
+    setIsAuthorized(checkUserAuthorization().authorized)
     async function fetchData() {
       const response = await WallManager.loadAllPosts();
       setPosts([...response.data])
@@ -23,37 +49,43 @@ const Wall = () => {
   function handleClickDeletePost(event, postId) {
     event.preventDefault();
     async function deletePost() {
+      history.push('/registeredsuccessfully')
       const response = await WallManager.deleteOnePost(postId);
       if (response.status === 204) {
         alert("Post successfully deleted")
-
       }
-      console.log("response click delete",response);
       setIsFetching(true);
     }
     deletePost();
-    
+  }
 
-    console.log("handle", postId);
+  function handleClickLogout(event) {
+    event.preventDefault();
+    deleteFromLocalStorage("WallAppToken")
   }
 
   return (
-
-
     <div className="container">
       <h1>Welcome to the Wall</h1>
+      <button
+              onClick={handleClickLogout}
+            >Logout</button>
+             <button
+              onClick={() => history.push(`/`)}
+            >home</button>
       {!isFetching && posts.map((post) => {
         return (<><h3>{post.content}</h3>
-          <button
-            onClick={() => history.push(`/publish`, { post })}
-          >Edit this post</button>
-          <button
-            onClick={(e) => {
-              handleClickDeletePost(e, post.id);
-            }}
-          >Delete this post</button>
+          {isAuthorized && <div >
+            <button
+              onClick={() => history.push(`/publish`, { post })}
+            >Edit this post</button>
+            <button
+              onClick={(e) => {
+                handleClickDeletePost(e, post.id);
+              }}
+            >Delete this post</button>
+          </div>}
         </>
-
         )
       })}
     </div>
