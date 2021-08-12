06d2a4616a711738e736eb22596519d7e72bfadb
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-08-12 20:24:18 -0300

    Cypress installed

---
+++ frontend/cypress/integration/loginTest.spec.js
@@ -0,0 +1,24 @@
+describe('Tests for Login', function () {
+  it('I should login successfully', () => {
+    cy.visit('http://localhost:3001/');
+
+    cy.get(':nth-child(1) > .form-control').type('test@test.com');
+
+    cy.get(':nth-child(2) > .form-control').type('test');
+
+    cy.get('.btn').click();
+
+    cy.url().should('contain', 'http://localhost:4100/');
+    cy.get(':nth-child(4) > .nav-link').should('have.attr', 'href', '/@test');
+    cy.get(':nth-child(3) > .nav-link').should(
+      'have.attr',
+      'href',
+      '/settings',
+    );
+    cy.get('.container > .nav > :nth-child(2) > .nav-link').should(
+      'have.attr',
+      'href',
+      '/editor',
+    );
+  });
+});
